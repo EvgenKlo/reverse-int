addacbc1b8a4c6098fe399a9995d3728b8f3fe17
EvgenKlo <klokovevgen@gmail.com>
2023-01-02 21:22:28 +0500

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let modulN = Math.abs(n);
+    let strModulN = `${modulN}`;
+    let reversStrModulN = Number(strModulN.split("").reverse().join(""));
+    return reversStrModulN;
 }
